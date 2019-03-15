0673283e17f976c224c1523e5b465a4d085beb21
Vvicii <vinceere@gmail.com>
2019-03-15 20:52:14 +0500

    Almost

--- webpage/index.php
+++ webpage/index.php
@@ -1,11 +1,70 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php 
+error_reporting(0);
+
+$name = $_REQUEST["name"];
+$email= $_REQUEST["email"];
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
+$confpassword= $_REQUEST["confpassword"];
+$dateofb= $_REQUEST["dateofb"];
+$gender= $_REQUEST["gender"];
+$marital= $_REQUEST["marital"];
+$address= $_REQUEST["address"];
+$city= $_REQUEST["city"];
+$postal_code= $_REQUEST["postal_code"];
+$home_phone= $_REQUEST["home_phone"];
+$mobile_phone= $_REQUEST["mobile_phone"];
+$credit_card_number= $_REQUEST["credit_card_number"];
+$credit_exp= $_REQUEST["credit_exp"];
+$m_salary= $_REQUEST["m_salary"];
+$url= $_REQUEST["url"];
+$gpa= $_REQUEST["gpa"];
+
+
+
+
+$isPost = $_SERVER["REQUEST_METHOD"]=="POST";
+$isGet = $_SERVER["REQUEST_METHOD"]=="GET";
+
+
+$isNameError = $isPost && !$name;
+$isEmailError = $isPost;
+$isUsernameError = $isPost;
+$isPasswordError = $isPost;
+$isConfpasswordError = $isPost;
+$isDateofbError = $isPost;
+$isGenderError = $isPost;
+$isMaritalError = $isPost;
+$isAddressError = $isPost;
+$isCityError = $isPost;
+$isPostalCodeError = $isPost;
+$isHomePhoneError = $isPost;
+$isMobilePhoneError = $isPost;
+$isCreditCardNumberError = $isPost;
+$isCreditExpError = $isPost;
+$isMSalaryError = $isPost;
+$isUrlError = $isPost;
+$isGpaError = $isPost;
+
+
+$isFormError; 
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr"> 
+	<meta charset="utf-8">
 	<head>
-		<title>Validating Forms</title>
+		<title>Registration form</title>
 		<link href="style.css" type="text/css" rel="stylesheet" />
 	</head>
 	
 	<body>
+
 		<h1>Registration Form</h1>
 
 		<p>
@@ -15,17 +74,50 @@
 		<hr />
 		
 		<h2>Please, fill below fields correctly</h2>
+		<form>
 		<dl>
-			<dt>Name</dt>
-			<dd>
-				???
-			</dd>
+			<dt>Name <span class="error"></span></dt>
+			<dd><input type="text" name="name" /></dd>
+			<dt>Email<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Username<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Password<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Confirm Password<span></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Date of birth<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Gender<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Marital status<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Address<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>City<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Postal Code<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Home Phone<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Mobile Phone<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Credit Card Number<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Credit Card Expiry Date<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Monthly Salary<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Web Site URL<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
+			<dt>Overall GPA<span class="error"></span></dt>
+			<dd><input type="text" /></dd>
 			
-			<!-- Write other fiels similar to Name as specified in lab6.pdf -->
 		</dl>
 		
-		<div>
-			Register
-		</div>
+		
+			 <input type="submit" value="Register">
+		
+		</form>
 	</body>
 </html>
